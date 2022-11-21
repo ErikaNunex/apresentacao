d873298d3d1710139720d73a95e5b208fd8e7e19
ErikaNunex <erikanunes0512@gmailcom>
2022-11-21 08:25:18 -0300

    ignorando linha

--- teste.php
+++ teste.php
@@ -1,12 +1,13 @@
 <?php
 // Lê um arquivo em um array.  Nesse exemplo nós obteremos o código fonte de
 // uma URL via HTTP
-$lines = file ('teste.txt');
+$lines = file ('teste.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
 
 // Percorre o array, mostrando o fonte HTML com numeração de linhas.
 foreach ($lines as $line_num => $line) {
     echo "Linha #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br>\n";
 }
+
 
 var_dump($lines);
 
